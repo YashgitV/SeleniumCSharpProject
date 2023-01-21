70050779250636f4ffe5627f893277a15564133d
YashgitV <kyaswanth816@gmail.com>
2023-01-21 12:39:10 +0530

    added index.html | modified test1.txt

---
+++ SeleniumSendkeys/Program.cs
@@ -0,0 +1,40 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SeleniumSendkeys
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver;
+            driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://mail.rediff.com/cgi-bin/login.cgi");
+            driver.Manage().Window.Maximize();
+            Thread.Sleep(4000);
+
+            //1st approach
+
+            IJavaScriptExecutor ij1 = (IJavaScriptExecutor)driver;
+            //ij1.ExecuteScript("document.getElementById('login1').value ='test'");
+            //Thread.Sleep(2000);
+
+            //2nd approach
+            IWebElement ele1 = driver.FindElement(By.Id("login1"));
+            ij1.ExecuteScript("arguments[0].value='test data';",ele1);
+            Thread.Sleep(2000);
+
+            //Click operation new apparoach
+            IWebElement clck = driver.FindElement(By.XPath("//input[@type='submit']"));
+            ij1.ExecuteScript("arguments[0].click();", clck);
+            //driver.Close();
+
+        }
+    }
+}
